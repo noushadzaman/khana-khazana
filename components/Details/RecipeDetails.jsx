494ea16350cb3ad84dcf2eff6d315ad309906149
Noushadozi <noushadozi333@gmail.com>
2024-05-01 18:49:58 +0600

    share in social medias

--- components/Details/RecipeDetails.jsx
+++ components/Details/RecipeDetails.jsx
@@ -5,15 +5,28 @@ import { useAuth } from "@/app/hooks/useAuth";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
+import {
+    FacebookShareButton,
+    FacebookIcon,
+    PinterestShareButton,
+    PinterestIcon,
+    RedditShareButton,
+    RedditIcon,
+    WhatsappShareButton,
+    WhatsappIcon,
+    LinkedinShareButton,
+    LinkedinIcon,
+} from 'next-share';
 
 const RecipeDetails = ({ recipe }) => {
     const { auth, setAuth } = useAuth();
     const alreadyFavourite = auth?.favourites.find(fav => fav == recipe?.id);
     const [isFavourite, setIsFavourite] = useState(alreadyFavourite);
     const [favourite, setFavourite] = useState(false);
+    const [btns, setBtns] = useState(false);
     const router = useRouter();
-    
-    
+
+
     async function addFavourite() {
         if (isFavourite) {
             return;
@@ -25,9 +38,9 @@ const RecipeDetails = ({ recipe }) => {
                 favourites: [...auth?.favourites, recipe?.id]
             });
             setFavourite(true);
-        }   
+        }
         else {
-            router.push('/login') 
+            router.push('/login')
         }
     }
 
@@ -83,7 +96,7 @@ const RecipeDetails = ({ recipe }) => {
                         </div>
                     </div>
 
-                    <div className="flex gap-4 justify-end">
+                    <div className="flex gap-4 justify-end items-center">
                         <div
                             onClick={addFavourite}
                             className={`flex gap-2 hover:text-[#eb4a36] ${isFavourite ? 'text-[#eb4a36] cursor-not-allowed' : 'text-gray-600 cursor-pointer'}`}>
@@ -95,23 +108,53 @@ const RecipeDetails = ({ recipe }) => {
                             </svg>
                             <span>Favourite</span>
                         </div>
-
-                        <div className="flex gap-2 text-gray-600 cursor-pointer hover:text-[#0E79F6]">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
-                                stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-                                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                                <path d="M6 12m-3 0a3 3 0 1 0 6 0a3 3 0 1 0 -6 0" />
-                                <path d="M18 6m-3 0a3 3 0 1 0 6 0a3 3 0 1 0 -6 0" />
-                                <path d="M18 18m-3 0a3 3 0 1 0 6 0a3 3 0 1 0 -6 0" />
-                                <path d="M8.7 10.7l6.6 -3.4" />
-                                <path d="M8.7 13.3l6.6 3.4" />
-                            </svg>
-                            <span>Share</span>
+                        <div className="flex flex-col items-center gap-3">
+                            {
+                                btns && <div>
+                                    <FacebookShareButton
+                                        url={`http://localhost:3000/details/${recipe?.id}`} >
+                                        <FacebookIcon size={32} round />
+                                    </FacebookShareButton>
+                                    <PinterestShareButton
+                                        url={`http://localhost:3000/details/${recipe?.id}`} >
+                                        <PinterestIcon size={32} round />
+                                    </PinterestShareButton>
+                                    <RedditShareButton
+                                        url={`http://localhost:3000/details/${recipe?.id}`} >
+                                        <RedditIcon size={32} round />
+                                    </RedditShareButton>
+                                    <WhatsappShareButton
+                                        url={`http://localhost:3000/details/${recipe?.id}`} >
+                                        <WhatsappIcon size={32} round />
+                                    </WhatsappShareButton>
+                                    <LinkedinShareButton
+                                        url={`http://localhost:3000/details/${recipe?.id}`} >
+                                        <LinkedinIcon size={32} round />
+                                    </LinkedinShareButton>
+                                </div>
+                            }
+                            {
+                                !btns &&
+                                <div
+                                    onClick={() => setBtns(true)}
+                                    className="flex gap-2 text-gray-600 cursor-pointer hover:text-[#0E79F6]">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
+                                        stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                                        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                                        <path d="M6 12m-3 0a3 3 0 1 0 6 0a3 3 0 1 0 -6 0" />
+                                        <path d="M18 6m-3 0a3 3 0 1 0 6 0a3 3 0 1 0 -6 0" />
+                                        <path d="M18 18m-3 0a3 3 0 1 0 6 0a3 3 0 1 0 -6 0" />
+                                        <path d="M8.7 10.7l6.6 -3.4" />
+                                        <path d="M8.7 13.3l6.6 3.4" />
+                                    </svg>
+                                    <span>Share</span>
+                                </div>
+                            }
                         </div>
                     </div>
                 </div>
             </div>
-        </section>
+        </section >
     );
 };
 
